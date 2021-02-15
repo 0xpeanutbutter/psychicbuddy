01b958efb75c8f6ca3a61155ee3498f3dbc6f26a
DragonPyli <codewizard001@gmail.com>
2021-02-15 16:45:08 +0530

    Temp Commit

--- Backend/PsychicBuddy/__init__.py
+++ Backend/PsychicBuddy/__init__.py
@@ -3,6 +3,8 @@ from .utils.summarization import getCategory
 
 from .utils.files import audioToText , videoToText , splitPassage
 
+from .utils.yolo import Yolo_v3
+
 from flask import Flask
 
 UPLOAD_FOLDER = './Media'
